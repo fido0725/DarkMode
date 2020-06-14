1a6b26593b3f747df68913515f2865695e80659c
fido0725 <chenfong_12@163.com>
2020-06-14 10:44:25 +0800

    dark mode for navbar

--- DarkMode/ViewController.m
+++ DarkMode/ViewController.m
@@ -16,8 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    if ([self.navigationController.viewControllers indexOfObject:self] != 0) {
+        [self addNavigationBarItem];
+    }else{
+        UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(50, 200, 100, 50)];
+        [btn setImage:[UIImage systemImageNamed:@"paperplane"] forState:UIControlStateNormal];
+        [btn setTintColor:[UIColor systemYellowColor]];
+        [btn addTarget:self action:@selector(toPush:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:btn];
+    }
+
 }
 
+-(void)addNavigationBarItem
+{
+    UIButton *backBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [backBtn setImage:[UIImage imageNamed:@"arrowback"] forState:UIControlStateNormal];
+    [backBtn setTitle:@"返回" forState: UIControlStateNormal];
+    [backBtn setTitleColor:[UIColor systemGrayColor] forState:UIControlStateNormal];
+    [backBtn addTarget:self action:@selector(toBack:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:backBtn];
+}
+
+-(void)toBack:(UIButton *)sender
+{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+-(void)toPush:(UIButton *)sender
+{
+    ViewController *vc = [[ViewController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
+}
 
 @end
